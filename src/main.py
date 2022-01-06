64c9a6eecc6df7383a70a264d51b70ec7ebadc31
RealTigerCZ <hucellj@seznam.cz>
2022-01-06 19:01:02 +0100

    Added 'Textures' dataclass and basic loop for window

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-import pygame
+import pygame, sys, os
 
 class Tile:
     def __init__(self, x, y):
@@ -14,15 +14,40 @@ class Tile:
             x = dir[0] + self.__x
             y = dir[1] + self.__y
             if x < glo.sizeX and x >= 0 and y < glo.SizeY and y >= 0:
-                self.neighbours.append(glo.board[y][x]) 
+                self.neighbours.append(glo.board[y][x])
+
+    def render(self, screen):
+        pass
+
+
+@dataclass
+class Textures:
+    path: str
+    extension: str = "png"
+
+    bomb_tile: str = "bomb_tile"
+    bomb_tile_red: str = "bomb_tile_red"
+    flag_tile: str = "flag_tile"
+    uncovered_tile: str = "uncovered_tile"
 
 
 @dataclass
 class Glo:
     sizeX: int
     sizeY: int
+    sizeTile: int
     bombsCount: int
+    textures: Textures
     board: list[list[Tile]] = field(default_factory=list)
 
-
-glo = Glo()
+run = True
+screen = pygame.display.set_mode((350, 250), pygame.RESIZABLE)
+clock = pygame.time.Clock()
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    screen.fill((0,255,255))
+    clock.tick(20)
+    print(screen.get_width(), screen.get_height())
+    pygame.display.flip()
